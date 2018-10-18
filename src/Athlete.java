c7d5f386dbdc12b148312ed5f2b6594b0ddd3c45
Declimator <league@iMac-5.attlocal.net>
2018-10-18 15:53:06 -0700

    RunningRace

--- src/Athlete.java
+++ src/Athlete.java
@@ -9,11 +9,22 @@ public class Athlete {
     int bibNumber;
 
 Athlete (String name, int speed){
+	raceLocation = "Boston";
+	bibNumber++;
     this.name = name;
     this.speed = speed;
 }
 
 public static void main(String[] args) {
     //create two athletes      //print their names, bibNumbers, and the location of their race. }
+Athlete one = new Athlete("test", 1);
+nextBibNumber=one.bibNumber+1;
+System.out.println(one.name);
+System.out.println(one.bibNumber);
+System.out.println(one.raceLocation);
+Athlete two = new Athlete("Joe", 50);
+System.out.println(two.name);
+System.out.println(one.nextBibNumber);
+System.out.println(two.raceLocation);
 }
 }
